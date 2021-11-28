8dea8495d7234ed271bd7eb69be445278f69c4a3
Abrammm <91841366+Gimboabrax@users.noreply.github.com>
2021-11-28 21:07:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+#include "lib.h"
+
+char conv(unsigned char let=0){
+    bool flag=false;
+    if(((let>64)&(let<91))or((let>96)&(let<123))){
+        if((let>64)&(let<91)){
+            let=let + ' ';
+        } else{
+            let=let - ' ';
+        }
+        flag=true;
+    } else{
+        flag=false;
+    }
+    return let;
+}
 
