db99cf59a0f880f31facd772dc70b3d99d0343a7
Abrammm <91841366+Gimboabrax@users.noreply.github.com>
2021-11-28 21:06:38 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char let;
+    cin >> let;
+    if(conv(let)){
+        cout << conv(let) << endl;
+    } else{
+        cout << "errore" << endl;
+    }
+    return 0;
 }
